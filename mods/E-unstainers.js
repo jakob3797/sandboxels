36b8a801934ce458799850a452cef098e517eb14
jakob3797 <167775393+jakob3797@users.noreply.github.com>
2024-05-07 12:47:33 -0500

    Create E-unstainers.js
    Tested working

---
+++ mods/E-unstainers.js
@@ -0,0 +1,108 @@
+elements.eunstainer_up2 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [
+         "XX|ST:-1|XX",
+         "XX|ST:-1|XX",
+         "XX|XX|XX",
+         "XX|XX|XX",
+         "XX|XX|XX",
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+ elements.eunstainer_up1 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|ST:-1|XX",
+         "XX|XX|XX",
+         "XX|XX|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+elements.eunstainer_down2 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [
+         "XX|XX|XX",
+         "XX|XX|XX",
+         "XX|XX|XX",
+         "XX|ST:-1|XX",
+         "XX|ST:-1|XX",
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+ elements.eunstainer_down1 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|XX|XX",
+         "XX|XX|XX",
+         "XX|ST:-1|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+elements.eunstainer_left2 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|XX|XX|XX|XX",
+         "ST:-1|ST:-1|XX|XX|XX",
+         "XX|XX|XX|XX|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+elements.eunstainer_left1 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|XX|XX",
+         "ST:-1|XX|XX",
+         "XX|XX|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+elements.eunstainer_right2 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|XX|XX|XX|XX",
+         "XX|XX|XX|ST:-1|ST:-1",
+         "XX|XX|XX|XX|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
+elements.eunstainer_right1 = {
+     behavior: behaviors.WALL,
+     behaviorOn: [ 
+         "XX|XX|XX",
+         "XX|XX|ST:-1",
+         "XX|XX|XX"
+     ],
+     color: "#4103fc", // Choose your desired color
+     category: "machines",
+     conduct: .75, // This defines how the element conducts electricity or energy, set as needed
+     hardness: 1,
+     state: "solid",
+ };
