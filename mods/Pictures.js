90efca4dca6c33ff4674098d3d09f111a0243d2f
jakob3797 <167775393+jakob3797@users.noreply.github.com>
2026-01-23 13:35:26 -0600

    Add Pictures.js
    Tested adds a Christian painting and the colors of the painting

---
+++ mods/Pictures.js
@@ -0,0 +1,168 @@
+elements.picture = {
+  color: "#aea6d8",
+  behavior: [
+    "CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_13|CR:color_13|CR:color_13|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_13|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_13|CR:color_13|CR:color_13|CR:color_13|CR:color_13|CR:color_11|CR:frame",
+    "CR:frame|CR:color_13|CR:color_13|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_5|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_5|CR:color_5|CR:color_5|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_5|CR:color_11|CR:color_11|CR:color_11|CR:color_15|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_11|CR:color_5|CR:color_11|CR:color_11|CR:color_11|CR:color_12|CR:color_11|CR:color_11|CR:color_11|CR:frame",
+    "CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame|CR:frame",
+  ],
+  category: "picture",
+  viscosity: 1,
+  reactions: {},
+  state: "solid",
+  density: 43
+};
+
+elements.frame = {
+  behavior: behaviors.SOLID,
+  color: "#999999",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_11 = {
+  behavior: behaviors.SOLID,
+  color: "#aea6d8",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_12 = {
+  behavior: behaviors.SOLID,
+  color: "#b46c88",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_13 = {
+  behavior: behaviors.SOLID,
+  color: "#e3e3e3",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_14 = {
+  behavior: behaviors.SOLID,
+  color: "#b58290",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_15 = {
+  behavior: behaviors.SOLID,
+  color: "#774838",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_16 = {
+  behavior: behaviors.SOLID,
+  color: "#a46464",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_17 = {
+  behavior: behaviors.SOLID,
+  color: "#b4accc",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_8 = {
+  behavior: behaviors.SOLID,
+  color: "#b48ca4",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_9 = {
+  behavior: behaviors.SOLID,
+  color: "#b4a4c9",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_10 = {
+  behavior: behaviors.SOLID,
+  color: "#bc9cb4",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_1 = {
+  behavior: behaviors.SOLID,
+  color: "#bbbadf",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_2 = {
+  behavior: behaviors.SOLID,
+  color: "#b46c88",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_3 = {
+  behavior: behaviors.SOLID,
+  color: "#a4787c",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_4 = {
+  behavior: behaviors.SOLID,
+  color: "#b58290",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_5 = {
+  behavior: behaviors.SOLID,
+  color: "#774838",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_6 = {
+  behavior: behaviors.SOLID,
+  color: "#a46464",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_7 = {
+  behavior: behaviors.SOLID,
+  color: "#b4accc",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_8 = {
+  behavior: behaviors.SOLID,
+  color: "#b48ca4",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_9 = {
+  behavior: behaviors.SOLID,
+  color: "#b4a4c9",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
+elements.color_10 = {
+  behavior: behaviors.SOLID,
+  color: "#bc9cb4",
+  category: "picture",
+  state: "solid",
+  hidden: false
+};
